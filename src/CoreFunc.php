e1298ba5ec526a093fea1fa7df500927b7d6476e
Pekka Laiho <pekka.i.laiho@gmail.com>
2020-06-03 19:39:11 +0700

    fix argument validation messages

--- src/CoreFunc.php
+++ src/CoreFunc.php
@@ -27,17 +27,15 @@ class CoreFunc extends Func
 
     private function validateArgs(int $count)
     {
-        if ($this->minArgs >= 0 && $count < $this->minArgs) {
-            if ($this->minArgs == $this->maxArgs) {
-                throw new MadLispException(sprintf("%s requires exactly %s argument%s", $this->name, $this->minArgs,
-                                                   $this->minArgs == 1 ? '' : 's'));
-            } else {
-                throw new MadLispException(sprintf("%s requires at least %s argument%s", $this->name, $this->minArgs,
-                                                   $this->minArgs == 1 ? '' : 's'));
-            }
+        if ($this->minArgs == $this->maxArgs && $count != $this->minArgs) {
+            throw new MadLispException(sprintf("%s requires exactly %s argument%s", $this->name, $this->minArgs,
+                                               $this->minArgs == 1 ? '' : 's'));
+        } elseif ($count < $this->minArgs) {
+            throw new MadLispException(sprintf("%s requires at least %s argument%s", $this->name, $this->minArgs,
+                                               $this->minArgs == 1 ? '' : 's'));
         } elseif ($this->maxArgs >= 0 && $count > $this->maxArgs) {
-                throw new MadLispException(sprintf("%s requires at most %s argument%s", $this->name, $this->maxArgs,
-                                                   $this->maxArgs == 1 ? '' : 's'));
+            throw new MadLispException(sprintf("%s requires at most %s argument%s", $this->name, $this->maxArgs,
+                                               $this->maxArgs == 1 ? '' : 's'));
         }
     }
 }
