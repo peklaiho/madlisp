01d429324ec8c983c644b49da0366f47b3ebe52a
Pekka Laiho <pekka.i.laiho@gmail.com>
2020-06-06 20:12:45 +0700

    save history for readline

--- repl.php
+++ repl.php
@@ -3,6 +3,12 @@ require('bootstrap.php');
 
 list($lisp, $rootEnv) = ml_get_lisp();
 
+// Read history
+$historyFile = $_SERVER['HOME'] . '/.madlisp_history';
+if (is_readable($historyFile)) {
+    readline_read_history($historyFile);
+}
+
 // Create new env for user definitions
 $userEnv = new MadLisp\Env('repl', $rootEnv);
 
@@ -11,6 +17,11 @@ while (true) {
 
     try {
         $lisp->rep($input, $userEnv);
+
+        if ($input) {
+            readline_add_history($input);
+            readline_write_history($historyFile);
+        }
     } catch (MadLisp\MadLispException $ex) {
         print('error: ' . $ex->getMessage());
     } catch (TypeError $ex) {
